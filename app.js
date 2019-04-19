268ec6b1d98657140d7c4c47d92e74d64d562d41
Sansevieria <elizabethleighmather@gmail.com>
2019-04-18 19:31:01 -0500

    working-ish

--- app.js
+++ app.js
@@ -15,46 +15,56 @@ $(document).ready(function () {
 
         // bird api
         var searchForBirds = function (lat, lon, newLoc) {
+
             var lat = `${lat}`;
             var lon = `${lon}`;
             var newLoc = [];
-            // console.log(newLoc);
-            // console.log(locationId);
-            console.log(lat, lon, newLoc);
+            var nearbyBirds = [];
 
-            // var queryURL = "https://ebird.org/ws2.0/data/obs/L4796501/recent?key=56oflln2kieu";
+            console.log(nearbyBirds);
+            console.log(newLoc);
+            // console.log(locationId);
+            // console.log(lat, lon, newLoc);
+
+            // locId L1150539
+            // var queryURL = "https://ebird.org/ws2.0/data/obs/L1150539/recent?key=56oflln2kieu";
             var queryURL = "https://ebird.org/ws2.0/ref/hotspot/geo?lat=" + lat + "&lng=" + lon + "&fmt=json&key=56oflln2kieu";
 
             $.ajax({
                 url: queryURL,
                 method: "GET"
             }).then(function (data) {
-                console.log(data);
+                // console.log(data);
 
                 for (var i = 0; i < data.length; i++) {
                     // console.log(data[i].locId);
                     var locationId = data[i].locId;
                 }
-                newLoc.push(locationId);
-                // console.log(newLoc);
-            }).then(function(locationId) {
-                // console.log(locationId);
 
-                var queryURL = "https://ebird.org/ws2.0/data/obs/" + newLoc + "/recent?key=56oflln2kieu";
-                console.log(queryURL);
+                newLoc.push(locationId);
+                // console.log(locationId);
+            }).then(function(data) {
+                // plug user's unique locId into ajax call for birds...
+                // found in that area.
+
+                // locId L1150539
+                var queryURL = "https://ebird.org/ws2.0/data/obs/L1150539/recent?key=56oflln2kieu";
+                // console.log(queryURL);
 
                 $.ajax({
                     url: queryURL,
                     method: "GET"
-                }).then(function(data) {
-                    console.log(data);
-                    var birdsNearby = [];
-                    var howMany = [];
-                    // console.log(birdsNearby);
+                }).then(function(response) {
+                    // console.log(response);
 
-                    birdsNearby.push(data.comName);
-                })
-            })
+                    for (var i = 0; i < response.length; i++) {
+                        console.log(response[i].comName);
+                        var birdList = response[i].comName;
+                    }
+
+                    nearbyBirds.push(birdList);
+                });
+            });
         };
         searchForBirds(geoLat, geoLon);
     });
